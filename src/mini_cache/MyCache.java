2aa51175964cfc57a1eb7d25fccfd84f923cdf53
remGigo <302801938@qq.com>
2020-02-20 20:31:48 +0800

    third_edition

--- src/mini_cache/MyCache.java
+++ src/mini_cache/MyCache.java
@@ -4,13 +4,15 @@ import mini_cache.computable.Computable;
 import mini_cache.computable.ExpensiveFunction;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 /**
  * 描述：  用ConcurrentHashMap保证线程安全，但是细细想来还有一个重复计算的问题，而且应该频率不低
  */
 public class MyCache<A, V> implements Computable<A, V> {
 
-    private final Map<A, V> cache = new ConcurrentHashMap<>();
+    private final Map<A, Future<V>> cache = new ConcurrentHashMap<>();
 
     private final Computable<A, V> c;
 
@@ -20,13 +22,15 @@ public class MyCache<A, V> implements Computable<A, V> {
 
     @Override
     public V compute(A arg) throws Exception {
-        System.out.println("进入缓存机制");
-        V result = cache.get(arg);
-        if (result == null) {
-            result = c.compute(arg);
-            cache.put(arg, result);
+        Future<V> f = cache.get(arg);
+        if (f == null) {   //虽然概率小 但是这儿其实还是拦不住多线程并发访问的
+            FutureTask<V> ft = new FutureTask<>(()->c.compute(arg));//lambda表达式就是一个实现了callable接口的任务实例
+            f = ft;
+            cache.put(arg, ft);   //先put后run
+            System.out.println("正在执行FutureTask中的callable任务...");
+            ft.run();
         }
-        return result;
+        return f.get();
     }
 
     public static void main(String[] args) throws Exception {
